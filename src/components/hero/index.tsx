a4fa08a6a0d6ab2dcbfd89495762ea43eee8fd8f
samcesa45 <samcesa45@gmail.com>
2025-11-19 06:12:37 +0100

    minor fix

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -11,8 +11,8 @@ import { Button } from '../ui/buttons';
 const images = [heroImg, heroImg2, heroImg3, heroImg4];
 export default function Hero() {
   return (
-    <div className="w-full relative min-h-screen overflow-hidden background-slideshow pt-[90px] md:pt-[110px]">
-      <div className="absolute inset-0 w-full h-full">
+    <div className="w-full relative  overflow-hidden pt-[90px] md:pt-[110px]">
+     <div className="absolute top-0 left-0 w-full h-[82vh] md:h-full background-slideshow">
         <Swiper
           modules={[Autoplay, EffectFade]}
           effect="fade"
@@ -39,18 +39,18 @@ export default function Hero() {
       <div className="relative z-3 flex flex-col justify-center min-h-[calc(100vh-90px)] md:min-h-[calc(100vh-110px)]">
         <div className="w-full max-w-7xl px-4 sm:px-6 md:px-8 lg:px-20 py-8">
           <div className="max-w-full md:max-w-[500px] lg:max-w-[1000px]">
-            <p className="text-white uppercase text-lg font-avenir font-extrabold tracking-[.15em] mb-5">
+            <p className="text-white uppercase text-base sm:text-lg font-avenir font-extrabold tracking-[.15em] mb-5">
               Together, we are building opportunities through scholarships,
               mentorship, and skill development.
             </p>
             <div className="h-full title-container lg:mb-5">
-              <h1 className="text-white text-[42px] lg:text-[90px] leading-[1.1em] font-avenir font-black">
+              <h1 className="text-white text-[32px] sm:text-[42px] lg:text-[90px] leading-[1.1em] font-avenir font-black">
                 Empowering Enugu North's Future Through Education
               </h1>
             </div>
           </div>
           <div className="btn mt-8 w-full xs:w-auto">
-            <Button className="bg-primary w-full absolute bottom-0 left-0 sm:relative xs:w-auto hover:bg-light-yellow! hover:text-black text-white font-avenir! font-bold text-base leading-5 px-10 py-[25px]! rounded-none border-0">
+            <Button className="bg-primary w-full xs:w-auto hover:bg-light-yellow! hover:text-black text-white font-avenir! font-bold text-base leading-5 px-10 py-[25px]! rounded-none border-0">
               Apply for support
             </Button>
           </div>
@@ -59,4 +59,5 @@ export default function Hero() {
     </div>
   );
 }
+
 
